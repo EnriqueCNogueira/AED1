95b79a90379928867ca3c0632514a9f6a3b6a1fe
Enrique Campos Nogueira <enrique.nogueirapro@gmail.com>
2024-11-11 16:38:14 -0300

    Add files via upload

---
+++ IdadeEmDias.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+    int dia, mes, ano, n, resto;
+    
+    scanf("%d", &n);
+    
+    ano = n/365;
+    resto = n%365;
+    
+    mes = resto/30;
+    dia = resto%30;
+ 
+    printf("%d ano(s)\n%d mes(es)\n%d dia(s)\n", ano, mes, dia);
+    
+    return 0;
+}
