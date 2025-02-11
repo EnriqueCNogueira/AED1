9f6af5750da690c2a45335c10fc52b1fc5936ee0
Enrique Campos Nogueira <enrique.nogueirapro@gmail.com>
2025-02-10 22:17:20 -0300

    Create 1051.c

---
+++ 1051.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    float x, i;
+
+    scanf("%f", &x);
+    
+    if (x <= 2000.0){
+        printf("Isento\n");
+        return 0;
+    }
+    else if (x > 2000.0 && x <= 3000.0){
+        i = (x - 2000.0) * 0.08;
+    }
+    else if (x > 3000.0 && x <= 4500.0){
+        i = (x - 3000.0) * 0.18 + 80;
+    }
+    else if (x > 4500.0){
+        i = (x - 4500.0) * 0.28 + 350;
+    }
+    
+    printf("R$ %.2f\n", i);
+
+    return 0;
+}
