cf9f383027d3503be34c6a315afe0bfec64dd4f7
Enrique Campos Nogueira <enrique.nogueirapro@gmail.com>
2025-02-10 22:41:40 -0300

    Create 1002.c

---
+++ 1002.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+ double pi, raio, area;
+  pi = 3.14159;
+  scanf("%lf", &raio);
+  
+  area = raio * raio * pi;
+  
+  printf("A=%.4lf\n", area);
+  
+    return 0;
+}
